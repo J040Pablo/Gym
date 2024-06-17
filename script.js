cc8435c5b92e0bac3219e2a907505dabf7e4fe95
João Pablo <aguadetorneira7@gmail.com>
2024-06-17 12:35:58 -0300

    Bugs Fixed

--- script.js
+++ script.js
@@ -1,177 +1,201 @@
 // Funções de redirecionamento para as páginas específicas de cada músculo
 function redirectToChestSite() {
-    window.location.href = "chest.html";
+  window.location.href = "chest.html";
 }
 
 function redirectToDeltoidsSite() {
-    window.location.href = "deltoids.html";
+  window.location.href = "deltoids.html";
 }
 
 function redirectToAbdomenSite() {
-    window.location.href = "abdomen.html";
+  window.location.href = "abdomen.html";
 }
 
 function redirectToBicepsSite() {
-    window.location.href = "biceps.html";
+  window.location.href = "biceps.html";
 }
 
 function redirectToQuadricepsSite() {
-    window.location.href = "quadriceps.html";
+  window.location.href = "quadriceps.html";
 }
 
 function redirectToQuadricepsEsqSite() {
-    window.location.href = "quadriceps.html";
+  window.location.href = "quadriceps.html";
 }
 
 function redirectToTrapeziusSite() {
-    window.location.href = "trapezius.html";
+  window.location.href = "trapezius.html";
 }
 
 function redirectToUpperBackSite() {
-    window.location.href = "upperBack.html";
+  window.location.href = "upperBack.html";
 }
 
 function redirectToTricepsSite() {
-    window.location.href = "triceps.html";
+  window.location.href = "triceps.html";
 }
 
 function redirectToLatissimusDorsiSite() {
-    window.location.href = "latissimusdorsi.html";
+  window.location.href = "latissimusdorsi.html";
 }
 
 function redirectToGlutesSite() {
-    window.location.href = "glutes.html";
+  window.location.href = "glutes.html";
 }
 
 function redirectToHamstringsSite() {
-    window.location.href = "hamstrings.html";
+  window.location.href = "hamstrings.html";
 }
 
 function redirectToCalfSite() {
-    window.location.href = "calf.html";
+  window.location.href = "calf.html";
 }
 
 function redirectToPanturrilhaSite() {
-    window.location.href = "calf.html";
+  window.location.href = "calf.html";
 }
 
 function redirectToPanturrilhaEsquerdaSite() {
-    window.location.href = "calf.html";
-}
-
-function redirectToLeftForearmSite() {
-    window.location.href = "ForearmWorkout.html";
+  window.location.href = "calf.html";
 }
 
 function redirectToForearmWorkoutPage() {
-    window.location.href = "ForearmWorkout.html";
-}
-
-function redirectToForearmWorkout2Page() {
-    window.location.href = "ForearmWorkout2.html";
-}
-
-function redirectToForearmWorkoutesq2Page() {
-    window.location.href = "ForearmWorkoutesq2.html";
-}
-
-function redirectToTricepsEsqSite() {
-    window.location.href = "triceps.html";
-}
-
-function leftCalfRedirect() {
-    window.location.href = "calf.html";
+  window.location.href = "ForeamWorkout.html";
 }
 
 function redirectToPage(page) {
-    window.location.href = `${page}.html`;
+  window.location.href = `${page}.html`;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const loginForm = document.getElementById('loginForm');
-    const registerForm = document.getElementById('registerForm');
-    const profilePage = window.location.pathname.includes('profile.html');
+  const muscleSections = document.querySelectorAll('.muscle');
   
-    if (loginForm) {
-      loginForm.addEventListener('submit', (e) => {
-        e.preventDefault();
-  
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('password').value;
-  
-        // Simulação de recuperação de usuários do localStorage
-        const users = JSON.parse(localStorage.getItem('users')) || [];
-        const currentUser = users.find(user => user.email === email && user.password === password);
-  
-        if (currentUser) {
-          // Se o login for bem-sucedido, salva o usuário atual no localStorage
-          localStorage.setItem('currentUser', JSON.stringify(currentUser));
-          // Redireciona para a página de perfil
-          window.location.href = 'profile.html';
-        } else {
-          // Se as credenciais estiverem incorretas, exibe uma mensagem de erro (pode ser ajustado conforme necessário)
-          alert('Login failed. Please check your credentials.');
-        }
+  muscleSections.forEach(section => {
+      section.addEventListener('click', () => {
+          switch(section.className.split(' ')[1]) {
+              case 'trapezius':
+                  redirectToTrapeziusSite();
+                  break;
+              case 'upperBack':
+                  redirectToUpperBackSite();
+                  break;
+              case 'triceps':
+              case 'triceps2':
+              case 'tricepsesq':
+                  redirectToTricepsSite();
+                  break;
+              case 'latissimusdorsi':
+                  redirectToLatissimusDorsiSite();
+                  break;
+              case 'glutes':
+                  redirectToGlutesSite();
+                  break;
+              case 'hamstrings':
+                  redirectToHamstringsSite();
+                  break;
+              case 'calf':
+              case 'panturrilha':
+              case 'panturrilha_Esquerda':
+              case 'left-calf':
+                  redirectToCalfSite();
+                  break;
+              case 'deltoids2':
+                  redirectToDeltoidsSite();
+                  break;
+              case 'quadriceps2':
+              case 'quadriceps':
+              case 'quadricepsEsq':
+                  redirectToQuadricepsSite();
+                  break;
+              case 'biceps2':
+              case 'biceps':
+              case 'left-biceps':
+                  redirectToBicepsSite();
+                  break;
+              case 'left-shoulder':
+              case 'right-shoulder':
+                  redirectToDeltoidsSite();
+                  break;
+              case 'antebraço':
+              case 'left-forearm':
+              case 'ForeamWorkout':
+              case 'ForeamWorkout2':
+              case 'ForeamWorkoutesq2':
+                  redirectToForearmWorkoutPage();
+                  break;
+              case 'chest':
+                  redirectToChestSite();
+                  break;
+              case 'abdomen':
+                  redirectToAbdomenSite();
+                  break;
+              case 'cabeca':
+              case 'cabecaAtras':
+                  // Adicione a função de redirecionamento correspondente aqui
+                  break;
+              case 'pe':
+              case 'peAtras':
+                  // Adicione a função de redirecionamento correspondente aqui
+                  break;
+              default:
+                  console.error('Classe de músculo não reconhecida:', section.className);
+          }
       });
-    } else {
-      console.error('Login form not found.');
-    }
-  
-    if (registerForm) {
-      registerForm.addEventListener('submit', (e) => {
-        e.preventDefault();
-  
-        const regName = document.getElementById('regName').value;
-        const regEmail = document.getElementById('regEmail').value;
-        const regPassword = document.getElementById('regPassword').value;
-  
-        const users = JSON.parse(localStorage.getItem('users')) || [];
-        const userExists = users.some(user => user.email === regEmail);
-  
-        if (userExists) {
-          alert('This email is already registered. Please log in.');
-          return;
-        }
-  
-        const newUser = { name: regName, email: regEmail, password: regPassword };
-        users.push(newUser);
-        localStorage.setItem('users', JSON.stringify(users));
-        alert('User registered successfully! Please log in to continue.');
-        registerForm.reset();
-      });
-    } else {
-      console.error('Register form not found.');
-    }
-  
-    if (profilePage) {
-      const loginLink = document.getElementById('loginLink');
-      const profileLink = document.getElementById('profileLink');
-      const usernameSpan = document.getElementById('username');
-      const emailSpan = document.getElementById('email');
-      const logoutButton = document.getElementById('logoutButton');
-  
-      // Verifica se há um usuário logado no localStorage
-      const currentUser = JSON.parse(localStorage.getItem('currentUser'));
-  
+  });
+
+
+  //==================================Login do Usuario=========================================================
+  const loginForm = document.getElementById('loginForm');
+  const registerForm = document.getElementById('registerForm');
+  const profilePage = window.location.pathname.includes('profile.html');
+
+  if (loginForm) {
+    loginForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+
+      // Simulação de recuperação de usuários do localStorage
+      const users = JSON.parse(localStorage.getItem('users')) || [];
+      const currentUser = users.find(user => user.email === email && user.password === password);
+
       if (currentUser) {
-        // Se houver um usuário logado, mostra o nome de usuário e email
-        usernameSpan.textContent = currentUser.name;
-        emailSpan.textContent = currentUser.email;
-        loginLink.style.display = 'none'; // Esconde o link de login
-        profileLink.style.display = 'inline'; // Mostra o link de perfil
+        // Se o login for bem-sucedido, salva o usuário atual no localStorage
+        localStorage.setItem('currentUser', JSON.stringify(currentUser));
+        // Redireciona para a página de perfil
+        window.location.href = 'profile.html';
       } else {
-        // Se não houver usuário logado, redireciona para a página de login apenas se não estiver nela
-        if (!window.location.href.includes('login.html')) {
-          window.location.href = 'login.html';
-        }
+        // Se as credenciais estiverem incorretas, exibe uma mensagem de erro (pode ser ajustado conforme necessário)
+        alert('Login falhou. Por favor, verifique suas credenciais.');
+      }
+    });
+  } else {
+    console.error('Formulário de login não encontrado.');
+  }
+
+  if (registerForm) {
+    registerForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      const regName = document.getElementById('regName').value;
+      const regEmail = document.getElementById('regEmail').value;
+      const regPassword = document.getElementById('regPassword').value;
+
+      const users = JSON.parse(localStorage.getItem('users')) || [];
+      const userExists = users.some(user => user.email === regEmail);
+
+      if (userExists) {
+        alert('Este email já está registrado. Por favor, faça login.');
         return;
       }
-  
-      // Adiciona evento de logout ao botão
-      logoutButton.addEventListener('click', () => {
-        localStorage.removeItem('currentUser'); // Remove o usuário logado do localStorage
-        window.location.href = 'login.html'; // Redireciona para a página de login
-      });
-    }
-  });
-  
+
+      const newUser = { name: regName, email: regEmail, password: regPassword };
+      users.push(newUser);
+      localStorage.setItem('users', JSON.stringify(users));
+      alert('Registro bem-sucedido. Agora você pode fazer login.');
+    });
+  } else {
+    console.error('Formulário de registro não encontrado.');
+  }
+});
